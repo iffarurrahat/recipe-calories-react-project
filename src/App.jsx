d0a80052acfaffc7450a4a20ae51eb919fac493b
iffarurrahat <iffarurrahat@gmail.com>
2024-05-10 03:28:40 +0600

    Setup files all processing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Container from "./components/ui/Container";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Header />
+      </Container>
+    </>
+  );
+}
+
+export default App;

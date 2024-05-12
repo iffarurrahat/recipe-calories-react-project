4c2014ccd15519a885f1a1be6beeae2306018c41
iffarurrahat <iffarurrahat@gmail.com>
2024-05-12 15:20:31 +0600

    Currently cooking design part done

--- src/components/Recipes/CookingItems.jsx
+++ src/components/Recipes/CookingItems.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 const CookingItems = ({ addNewRecipe }) => {
-  console.log(addNewRecipe.length);
   return (
     <>
+      {/* Want to cook: part */}
       <div>
         <h2 className="text-2xl font-medium text-center mt-6">
           Want to cook: {addNewRecipe.length}
@@ -45,6 +45,42 @@ const CookingItems = ({ addNewRecipe }) => {
                 </tr>
               </tbody>
             )}
+          </table>
+        </div>
+      </div>
+
+      {/* Currently cooking: part */}
+      <div className="mt-8">
+        <h2 className="text-2xl font-medium text-center">
+          Currently cooking: 02
+        </h2>
+        <p className="h-[1px] w-4/5 bg-[#28282826] mx-auto mt-4 mb-6"></p>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Time</th>
+                <th>Calories</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr className="bg-base-200">
+                <th>1</th>
+                <td>Spaghetti Bolognese</td>
+                <td>30 minutes</td>
+                <td>600 calories</td>
+              </tr>
+              <tr className="">
+                <th></th>
+                <td></td>
+                <td>Total Time = 45 minutes</td>
+                <td>Total Calories = 1050 calories</td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>

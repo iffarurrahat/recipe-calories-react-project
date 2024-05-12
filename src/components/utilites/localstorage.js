e76c379c1f31e847d206ea660a77e08fb339e287
iffarurrahat <iffarurrahat@gmail.com>
2024-05-12 23:54:27 +0600

    utilites file add with local Storage

---
+++ src/components/utilites/localstorage.js
@@ -0,0 +1,27 @@
+const getStoredCart = () => {
+  const storeCartString = localStorage.getItem("cart");
+  if (storeCartString) {
+    return JSON.parse(storeCartString);
+  }
+  return [];
+};
+
+const saveCartToLS = (cart) => {
+  const cartStringified = JSON.stringify(cart);
+  localStorage.setItem("cart", cartStringified);
+};
+
+const addToLS = (id) => {
+  const cart = getStoredCart();
+  cart.push(id);
+  // save cart to localStorage
+  saveCartToLS(cart);
+};
+
+const removeFromLS = (id) => {
+  const cart = getStoredCart();
+  const remaining = cart.filter((idx) => idx !== id);
+  saveCartToLS(remaining);
+};
+
+export { addToLS, getStoredCart, removeFromLS };

99fcfe19bb43c9120f0b29b0b012d2ada1f726fd
iffarurrahat <iffarurrahat@gmail.com>
2024-05-13 05:57:34 +0600

    Footer Backgroun color Animated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,17 +6,38 @@ const Footer = () => {
     <div className=" mt-10 text-center py-32 footer">
       <Container>
         <div className="grid grid-cols-1 md:grid-cols-4">
-          <div className="bg-red-500 w-full h-20">
-            <p>
+          <div className="w-full h-20">
+            <p className="text-white">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Necessitatibus unde quisquam tempore incidunt officia, quo
               delectus eveniet voluptatem iure rem voluptas hic debitis,
               reiciendis at sunt maiores harum? Hic, ad!
             </p>
           </div>
-          <div className="bg-green-500 w-full h-20"></div>
-          <div className="bg-yellow-500 w-full h-20"></div>
-          <div className="bg-blue-500 w-full h-20"></div>
+          <div className=" w-full h-20">
+            <p className="text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus unde quisquam tempore incidunt officia, quo
+              delectus eveniet voluptatem iure rem voluptas hic debitis,
+              reiciendis at sunt maiores harum? Hic, ad!
+            </p>
+          </div>
+          <div className=" w-full h-20">
+            <p className="text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus unde quisquam tempore incidunt officia, quo
+              delectus eveniet voluptatem iure rem voluptas hic debitis,
+              reiciendis at sunt maiores harum? Hic, ad!
+            </p>
+          </div>
+          <div className=" w-full h-20">
+            <p className="text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus unde quisquam tempore incidunt officia, quo
+              delectus eveniet voluptatem iure rem voluptas hic debitis,
+              reiciendis at sunt maiores harum? Hic, ad!
+            </p>
+          </div>
         </div>
       </Container>
     </div>

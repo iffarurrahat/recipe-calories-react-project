d80d6b0854be99541660dabdc031ab6f729740f7
iffarurrahat <iffarurrahat@gmail.com>
2024-05-13 22:25:55 +0600

    added new section Subscribe to our Newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import OurNewsletter from "./components/OurNewsletter/OurNewsletter";
 import Recipes from "./components/Recipes/Recipes";
 import RecipesCategory from "./components/RecipesCategory/RecipesCategory";
 import Container from "./components/ui/Container";
@@ -15,6 +16,7 @@ function App() {
         <Recipes />
         <RecipesCategory />
       </Container>
+      <OurNewsletter />
       <Footer />
     </>
   );
---
+++ src/components/OurNewsletter/OurNewsletter.jsx
@@ -0,0 +1,23 @@
+import Container from "../ui/Container";
+import "./OurNewsletter.css";
+
+const OurNewsletter = () => {
+  return (
+    <div className="newsLatterBgImg h-96 md:h-[500px] mt-20 flex items-center text-white">
+      <Container>
+        <h2 className="text-2xl sm:text-3xl lg:text-4xl font-semibold">
+          Subscribe to our Newsletter
+        </h2>
+        <p className="md:w-1/2 left-7 my-4">
+          Fusce id velit placerat, efficitur libero placerat, sodales ante.
+          Curabitur sed erosat orci congue vestibulum.
+        </p>
+        <button className="bg-green-400 py-2 px-6 rounded-full">
+          Subscribe
+        </button>
+      </Container>
+    </div>
+  );
+};
+
+export default OurNewsletter;
